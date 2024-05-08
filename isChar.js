e48ff35de32e57381690fab9004505e130db3b06
queenbestill <inglescanarias@gmail.com>
2024-05-08 12:15:47 +0100

    IP-Strings-Functions

---
+++ isChar.js
@@ -0,0 +1,9 @@
+function isCharacterAVowel(char) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isCharacterAVowel("a"));
---
+++ maxOfThree.js
@@ -0,0 +1,13 @@
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
+        return num2;
+    } else if (num3 > num1 && num3 > num2) {
+        return num3;
+    } else {
+        return "they are equal";
+    }
+}
+
+console.log(maxOfThree(4, 8, 9)); // Output: 9
