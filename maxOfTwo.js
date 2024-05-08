08170b124ff6a89e659170ce6475d4f1611230d4
castiblancoely <167689350+CastiblancoEly@users.noreply.github.com>
2024-05-08 20:53:21 +0100

    Modificado
    Mejore la sintaxis.

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,12 +1,12 @@
 let numero1 = 10;
 let numero2 = 20;
 
-function maxOfTwoNumbers (numero1, numero2) {
-    if(numero1 < numero2) {
+function maxOfTwoNumbers(numero1, numero2) {
+    if (numero1 < numero2) {
         return numero2;
-    }else{return numero1;}
+    } else {
+        return numero1;
+    }
 }
 
 console.log(maxOfTwoNumbers(numero1, numero2));
-
-
