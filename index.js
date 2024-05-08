7a72b2e5ed654997dfecbc6217391851531accd7
Raquel <raqueldejorgetremps@gmail.com>
2024-05-08 12:09:44 +0100

    ejerciciodeporlamañanadía8

---
+++ index.js
@@ -0,0 +1,26 @@
+function maxOfTwoNumbers(x,y) {
+    if (x<y) {
+console.log (y)
+    } else {console.log (x)} 
+} 
+maxOfTwoNumbers (5,6)
+
+function maxOfThree(x,y,z) {
+    if (x<y<z) {
+        console.log (z)
+    } else {
+        console.log (y)
+    }
+}
+maxOfThree (5,6,7
+
+) 
+function isCharacterAVowel(letra) {
+    if (letra === 'a' || letra === 'e'){
+        return true
+    }else{
+        false
+    }
+}
+
+console.log(isCharacterAVowel('a'))
