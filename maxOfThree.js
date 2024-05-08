9d8a304f33af5d8d081e2e064d0e80222ad9e1b8
acozayda <traductora@gmail.com>
2024-05-08 12:18:10 +0100

    1 y medio

---
+++ maxOfThree.js
@@ -0,0 +1,14 @@
+let number1 = 3
+let number2 = 6
+
+function maxOfTwoNumbers (number1, number2) {
+    if(number1 < number2) {
+    return number2
+    }
+    else{
+        return number1
+    }
+}
+console.log(maxOfTwoNumbers(number1, number2) )
+
+function
