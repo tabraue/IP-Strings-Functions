e7fe77c46a47413d374607742bb70525b4d09b77
PaulaSLT <paulasanchez.lt@gmail.com>
2024-05-08 12:11:23 +0100

    Ejercicio maxOfThree

---
+++ maxOfThree.js
@@ -0,0 +1,15 @@
+function maxOfThree (x, y, z){
+if (x > y){
+    return ("Eres guay")
+}
+else if (z < y)
+    {return ("Eres muy guay")}
+else (x = y)
+{return ("No eres guay")}
+}
+
+let x = 5
+let y = 8
+let z = 2
+
+console.log(maxOfThree(x)) .
