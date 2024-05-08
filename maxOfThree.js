6dc4a92c4acb7232d35fa91f2211d41e72fb6ff7
Francisreboot <fmrb@gmail.com>
2024-05-08 12:13:17 +0100

    este es el js del ejercio 3

---
+++ maxOfThree.js
@@ -0,0 +1,11 @@
+function maxofThreeNumbers( x ,y ,z){
+    if (x > y ,z) {
+ return x;
+    }else { 
+        return y ,z;
+    else 
+
+    }
+} 
+
+console.log(maxofThreeNumbers(3 , 2 ,4))
---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+function maxofTwoNumbers( x ,y){
+    if (x>y) {
+ return x;
+    }else { 
+        return y;
+
+    }
+} 
+
+console.log(maxofTwoNumbers(3 , 2))
