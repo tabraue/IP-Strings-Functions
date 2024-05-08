9744f7dab2ed160ac0bd936d1e98c37f01cb3948
JCCH13 <juliocesarcordero@gmail.com>
2024-05-08 12:18:44 +0100

    en el ejercicio2

---
+++ index.js
@@ -0,0 +1,32 @@
+let num1 = 5 
+let num2 = 2
+
+// función de maxOfTwoNumbers
+function sayHi (num1, num2){
+    if (num1 > num2) {
+        return num1 
+    } else {
+        return num2
+    }
+}
+
+console.log(sayHi(num1, num2))
+
+
+
+let num1 = 5 
+let num2 = 2
+let num = 1
+
+// función de maxOfTwoNumbers
+function sayHi (num1, num2, num3){
+    if (num1 > num2 && num1> num3) {
+        return num1 
+    } else {
+        return num2
+    } return num3
+}
+
+console.log(sayHi(num1, num2, num3))
+
+
