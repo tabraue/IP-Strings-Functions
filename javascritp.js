047de2f52f6d0710aec01db4226ad7b439a309a2
noemimd92 <n8834219@gmail.com>
2024-05-13 16:02:29 +0100

    done

--- javascritp.js
+++ javascritp.js
@@ -1,3 +1,5 @@
+//MAXOFTWONUMBERS
+
 function maxOfTwoNumbers(a,b){
     if (a>b){
     return a;
@@ -7,7 +9,8 @@ function maxOfTwoNumbers(a,b){
     }
     console.log(maxOfTwoNumbers(15,12));
     
-    
+    //MAXOFTHRE3E
+
     function maxOThree( a,b,c) {
         if (a>b && a >c){
             return a;
@@ -19,3 +22,14 @@ function maxOfTwoNumbers(a,b){
         }
     }
     console.log(maxOThree(5,2,8))
+    console.log(maxOThree(8.5,2))
+
+    //ISCHARACTERAVWEL
+
+    const isCharacterAVowel = (char) => {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        return vowels.includes(char.toLowerCase());
+    }
+    // Test de función
+    console.log(isCharacterAVowel('a')); // true
+    console.log(isCharacterAVowel('b')); // false
